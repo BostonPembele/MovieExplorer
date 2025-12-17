f80bd6d6b3045f4eec506593ee77f86cba6cfe00
Boston Pembele <bostonpembele8@gmail.com>
2025-12-17 21:19:31 +0000

    settings page xaml

--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -1,9 +1,22 @@
+using MovieExplorer.Services;
+
 namespace MovieExplorer.Pages;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
-	{
-		InitializeComponent();
-	}
+    public SettingsPage()
+    {
+        InitializeComponent();
+
+        ThemePicker.ItemsSource = new List<string> { "System", "Light", "Dark" };
+
+        var saved = ThemeService.GetSavedTheme();
+        ThemePicker.SelectedItem = saved;
+    }
+
+    private void OnThemeChanged(object sender, EventArgs e)
+    {
+        var theme = ThemePicker.SelectedItem?.ToString() ?? "System";
+        ThemeService.ApplyTheme(theme);
+    }
 }
